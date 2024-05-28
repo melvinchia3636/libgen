22876b3983926dc0389884c674393c13698ed822
melvinchia3636 <melvinchia623600@gmail.com>
2024-05-28 22:08:44 +0800

    some more fixes

--- src/lib/components/search/fields/SortDirection.svelte
+++ src/lib/components/search/fields/SortDirection.svelte
@@ -2,14 +2,14 @@
 	import { sortDirection } from '$lib/store/search';
 </script>
 
-<li class="flex flex-col md:flex-row items-center justify-between p-2 py-4 gap-6 lg:gap-12">
-	<div class="w-full">
+<li class="flex flex-col md:flex-row items-start md:items-center justify-between p-2 py-4 gap-6 lg:gap-12">
+	<div >
 		<h2>Sort Direction</h2>
 		<p class="text-zinc-400 dark:text-zinc-500 text-sm font-light">
 			Select whether to sort ascending or descending.
 		</p>
 	</div>
-	<div class="w-full md:w-80 flex flex-wrap items-center gap-x-12 gap-y-4 justify-start lg:justify-end">
+	<div class="flex flex-wrap items-center gap-x-12 gap-y-4 justify-start md:justify-end">
 		<div class="block min-h-[1.5rem] pl-[1.5rem]">
 			<input
 				class="relative float-left mt-[0.2rem] mr-1 -ml-[1.5rem] h-[1.2rem] w-[1.2rem] appearance-none rounded-full border-2 border-solid border-zinc-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-orange-500 checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-orange-500 checked:after:bg-orange-500 checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 checked:focus:border-orange-500 checked:focus:before:scale-100 checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-zinc-700 dark:checked:border-orange-500 dark:checked:after:border-orange-500 dark:checked:after:bg-orange-500 dark:checked:focus:border-orange-500"
